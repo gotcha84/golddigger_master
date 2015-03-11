5893b6878c3b3c680a92c4e82b459c60ba71e7eb
gotcha84 <akalavakunta@gmail.com>
2015-03-11 00:20:47 -0500

    finished node with classifier

--- src/detect_coins.cpp
+++ src/detect_coins.cpp
@@ -1,17 +1,140 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
 #include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <string>
+#include <iostream> 
+#include <cctype>
+#include <iterator>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
 
 
-int main(int argc, char** argv)
-{
+class CoinDetector {
+
+    public:
+
+        string faceCascadeName = "/home/viki/catkin_ws/src/golddigger_master/src/coin.xml";
+        CascadeClassifier faceCascade;
+
+        static const float scale = 2.0;
+
+        CoinDetector() {
+            if (!faceCascade.load(faceCascadeName)) {
+                ROS_ERROR("ERROR: Could not load face classifier cascade");
+            }
+        }
+
+        ~CoinDetector() {
+
+        }
+
+        void detectAndDraw(Mat& img)
+        {
+            int i = 0;
+            double t = 0;
+            vector<Rect> faces;
+            static const Scalar colors[] =  { CV_RGB(0,0,255),
+                CV_RGB(0,128,255),
+                CV_RGB(0,255,255),
+                CV_RGB(0,255,0),
+                CV_RGB(255,128,0),
+                CV_RGB(255,255,0),
+                CV_RGB(255,0,0),
+                CV_RGB(255,0,255)} ;
+            Mat gray;
+
+            cvtColor( img, gray, CV_BGR2GRAY );
+            equalizeHist( gray, gray );
+
+            t = (double)cvGetTickCount();
+            faceCascade.detectMultiScale( gray, faces,
+                1.1, 2, 0
+                //|CV_HAAR_FIND_BIGGEST_OBJECT
+                //|CV_HAAR_DO_ROUGH_SEARCH
+                |CV_HAAR_SCALE_IMAGE
+                ,
+                Size(30, 30) );
+            t = (double)cvGetTickCount() - t;
+            printf( "detection time = %g ms\n", t/((double)cvGetTickFrequency()*1000.) );
+            for( vector<Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ )
+            {
+                Mat smallImgROI;
+                Point center;
+                Scalar color = colors[i%8];
+                int radius;
+
+                double aspect_ratio = (double)r->width/r->height;
+                if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
+                {
+                    center.x = cvRound((r->x + r->width*0.5)*scale);
+                    center.y = cvRound((r->y + r->height*0.5)*scale);
+                    radius = cvRound((r->width + r->height)*0.25*scale);
+                    circle( img, center, radius, color, 3, 8, 0 );
+                }
+                else
+                {
+                    rectangle( img, cvPoint(cvRound(r->x*scale), cvRound(r->y*scale)),
+                               cvPoint(cvRound((r->x + r->width-1)*scale), cvRound((r->y + r->height-1)*scale)),
+                               color, 3, 8, 0);
+                }
+            }
+        }
+
+};
+
+
+// ---------- end class definition
+
+
+image_transport::Subscriber sub;
+image_transport::Publisher pub;
+
+CoinDetector coinDetector;
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+
+    cout << "image callback!" << endl;
+
+    cv_bridge::CvImagePtr cv_ptr;
+    try {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // do face detect stuff
+    coinDetector.detectAndDraw(cv_ptr->image);
+
+    // cv_bridge::CvImage out_msg;
+    // //out_msg.header   = in_msg->header; // Same timestamp and tf frame as input image
+    // out_msg.encoding = sensor_msgs::image_encodings::BGR8; // Or whatever
+    // out_msg.image = cv_ptr->image; // Your cv::Mat
+    pub.publish(cv_ptr->toImageMsg());
+}
+
+int main(int argc, char** argv) {
+
     ros::init(argc, argv, "detect_coins");
     ros::NodeHandle n;
     
+    image_transport::ImageTransport it (n);
+    sub = it.subscribe("/camera/visible/image", 1, imageCallback);
+    pub = it.advertise("/output_video", 1);
+
     ros::Rate loopRate(10);
     
     while (ros::ok()) {
 
-
-        
         ros::spinOnce();
         loopRate.sleep();
         
