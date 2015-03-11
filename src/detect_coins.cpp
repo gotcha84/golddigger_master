6f293d96a6c2ad70757ff37a28681c108e9309e5
Jack Tomlinson <jack@jtcomputing.net>
2015-03-11 17:23:01 -0500

    modified to use HoughCircles to detect coins

--- src/detect_coins.cpp
+++ src/detect_coins.cpp
@@ -24,15 +24,13 @@ class CoinDetector {
 
     public:
 
-        string faceCascadeName = "/home/viki/catkin_ws/src/golddigger_master/src/coin.xml";
+        //string faceCascadeName = "/home/viki/catkin_ws/src/golddigger_master/src/coin.xml";
         CascadeClassifier faceCascade;
 
         static const float scale = 2.0;
 
         CoinDetector() {
-            if (!faceCascade.load(faceCascadeName)) {
-                ROS_ERROR("ERROR: Could not load classifier cascade");
-            }
+            namedWindow( "Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE );
         }
 
         ~CoinDetector() {
@@ -41,58 +39,31 @@ class CoinDetector {
 
         void detectAndDraw(Mat& img)
         {
-            int i = 0;
-            double t = 0;
-            vector<Rect> faces;
-            static const Scalar colors[] =  { CV_RGB(0,0,255),
-                CV_RGB(0,128,255),
-                CV_RGB(0,255,255),
-                CV_RGB(0,255,0),
-                CV_RGB(255,128,0),
-                CV_RGB(255,255,0),
-                CV_RGB(255,0,0),
-                CV_RGB(255,0,255)} ;
             Mat gray;
+            vector<Vec3f> circles;
 
             cvtColor( img, gray, CV_BGR2GRAY );
-            equalizeHist( gray, gray );
 
-            t = (double)cvGetTickCount();
-            faceCascade.detectMultiScale( gray, faces,
-                1.1, 2, 0
-                //|CV_HAAR_FIND_BIGGEST_OBJECT
-                //|CV_HAAR_DO_ROUGH_SEARCH
-                |CV_HAAR_SCALE_IMAGE
-                ,
-                Size(30, 30) );
-            t = (double)cvGetTickCount() - t;
-            // printf( "detection time = %g ms\n", t/((double)cvGetTickFrequency()*1000.) );
-            
-            for( vector<Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ )
+            // Blur so we don't get false positives
+            GaussianBlur( gray, gray, Size(9, 9), 2, 2 );
+
+            /// Apply the Hough Transform to find the circles
+            HoughCircles( gray, circles, CV_HOUGH_GRADIENT, 2, 100);
+
+            /// Draw the circles detected
+            for( size_t i = 0; i < circles.size(); i++)
             {
-                Mat smallImgROI;
-                Point center;
-                Scalar color = colors[i%8];
-                int radius;
+                Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+                int radius = cvRound(circles[i][2]);
 
-                double aspect_ratio = (double)r->width/r->height;
-                if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
-                {
-                    center.x = cvRound((r->x + r->width*0.5)*scale);
-                    center.y = cvRound((r->y + r->height*0.5)*scale);
-                    radius = cvRound((r->width + r->height)*0.25*scale);
-                    circle( img, center, radius, color, 3, 8, 0 );
-                }
-                else
-                {
-                    rectangle( img, cvPoint(cvRound(r->x*scale), cvRound(r->y*scale)),
-                               cvPoint(cvRound((r->x + r->width-1)*scale), cvRound((r->y + r->height-1)*scale)),
-                               color, 3, 8, 0);
-                }
+                // circle outline
+                circle( img, center, radius, Scalar(0,0,255), 3, 8, 0 );
             }
 
+            cout << "# circles: " << circles.size() << endl;
+
             std_msgs::Int64 msg;
-            if (faces.begin() != faces.end()) {
+            if (circles.size() > 0) {
                 msg.data = 1;
             }
             else {
@@ -116,7 +87,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 
     cv_bridge::CvImagePtr cv_ptr;
     try {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
     }
     catch (cv_bridge::Exception& e) {
       ROS_ERROR("cv_bridge exception: %s", e.what());
